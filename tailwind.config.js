bde41e8c6f3eadb0d9d0ec48281beb3c00ad1996
Edvaldo Afonso <eadafonso@gmail.com>
2022-07-02 09:23:49 +0100

    feat: init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        "accent-1": "#333",
+      },
+    },
+  },
+  plugins: [],
+};
