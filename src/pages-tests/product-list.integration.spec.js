e881be7c26ad7e9aecbef24b5825e21bae03bb03
Edvaldo Afonso <eadafonso@gmail.com>
2022-07-06 19:21:09 +0100

    feat: configure mirage

--- src/pages-tests/product-list.integration.spec.js
+++ src/pages-tests/product-list.integration.spec.js
@@ -2,11 +2,23 @@ import { screen, render, waitFor } from "@testing-library/react";
 
 import ProductList from "../pages";
 
+import { makeServer } from "../miragejs/server";
+
 const renderProductList = () => {
   render(<ProductList />);
 };
 
 describe("Product List", () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: "test" });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it("should render product list", () => {
     renderProductList();
 
@@ -14,10 +26,12 @@ describe("Product List", () => {
   });
 
   fit("sould render the productCard component 10 time", async () => {
+    server.createList("product", 10);
+
     renderProductList();
 
     await waitFor(() => {
-      expect(screen.getByTestId("product-card")).toHaveLength(10);
+      expect(screen.getAllByTestId("product-card")).toHaveLength(10);
     });
   });
 });
